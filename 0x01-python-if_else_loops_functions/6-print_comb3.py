d85c673f1ca68fdb9ef8bcf86ec413ca3335332c
solotole <solomontoleak47@gmail.com>
2024-01-16 20:22:18 -0800

    printing posible combinations of 2 digits in ascending order

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 10, 1):
     for b in range(0, 10, 1):
-        if (a < b and a != b) and (a != 8 and b != 9):
+        if (a < b and a != b) or (a != 8 and b != 9):
             print("{0:d}{1:d}".format(a, b), end=", ")
         elif a == 8 and b == 9:
             print("{0:d}{1:d}".format(a, b))
