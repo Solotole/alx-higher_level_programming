449abbb47992ca213a23c26040f1de668aaf982b
solotole <solomontoleak47@gmail.com>
2024-01-16 19:06:12 -0800

    printimg last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-value = int(number / 10)
-value2 = number - (value * 10)
+if number < 0:
+    value2 = number % -10
+else:
+    value2 = number % 10
+string1 = "Last digit of {0:d}i is {1:d} and is greater than 5"
+string2 = "Last digit of {0:d} is {1:d} and is less than 6 and not 0"
 if value2 > 5:
-    print("Last digit of {0:d}i is {1:d} and is greater than 5".format(number, value2))
+    print(string1.format(number, value2))
 elif value2 == 0:
     print("Last digit of {0:d} is {1:d} and is 0".format(number, value2))
 elif value2 < 6 and value2 != 0:
-    print("Last digit of {0:d} is {1:d} and is less than 6 and not 0".format(number, value2))
+    print(string2.format(number, value2))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-for i in range(0, 100, 1):
-    if i != 99:
-        print("{0:d}".format(i), end=", ")
-    else:
-        print("{0:d}".format(i))
+def last_digit(number):
+    print("{0:d}".format(number))
 
+for i in range(0, 100, 1):
+    if i < 99:
+        if len(i) == 1:
+            print("0{0:d}".format(i), end=", ")
+        else:
+            print("{0:d}".format(i), end=", ")
+    elif i == 99:
+        last_digit(i)
+    else:
+        pass
