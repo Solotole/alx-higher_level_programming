57f6ff63e222c5cd1bd929b57b3a0abf4a40724a
solotole <solomontoleak47@gmail.com>
2024-01-17 01:26:58 -0800

    printing posible combinations of 2 digits in ascending order

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+def determinant(s):
+    if s >= 97 and s <= 122:
+        return True
+    if s < 97 or s > 122:
+        return False
+
+
 def uppercase(str):
-    for i in range(len(str)):
+    size = len(str)
+    for i in range(size):
         string = ord(str[i])
-        if (string > 97 and string < 122) or (string):
-            print("{0:c}".format(string - 32), end="")
+        if (string >= 97 and string <= 122) or (string < 97 or string > 122):
+            if i != (size - 1):
+                print("{:c}".format(string - 32 if determinant(string) else string), end="")
+            if i == (size - 1):
+                print("{0:c}".format(string - 32))
         else:
-            print("{0:c}".format(string))
+            pass
