a42e7534458a0879b68d1c03d9079ad204492072
solotole <solomontoleak47@gmail.com>
2024-01-16 18:05:33 -0800

    printimg last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-value = number % 10
-if value > 5:
-    print("Last digit of {0:d} is {1:d} and is greater than 5".format(number, value))
-elif value == 0:
-    print("Last digit of {0:d} is {1:d} and is 0".format(number, value))
-elif value < 6 and value != 0:
-    print("Last digit of {0:d} is {1:d} and is less than 6 and not 0".format(number, value))
+value = int(number / 10)
+value2 = number - (value * 10)
+if value2 > 5:
+    print("Last digit of {0:d}i is {1:d} and is greater than 5".format(number, value2))
+elif value2 == 0:
+    print("Last digit of {0:d} is {1:d} and is 0".format(number, value2))
+elif value2 < 6 and value2 != 0:
+    print("Last digit of {0:d} is {1:d} and is less than 6 and not 0".format(number, value2))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         string = ord(str[i])
-        if string > 97 and string < 122:
-            if i != (len(str) - 1):
-                print("{0:c}".format(string - 32), end="")
-            else:
-                print("{0:c}".format(string - 32))
+        if (string > 97 and string < 122) or (string):
+            print("{0:c}".format(string - 32), end="")
         else:
-            continue
+            print("{0:c}".format(string))
