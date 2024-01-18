9e83d50ced0a4593b0f60fbef3d682a0aad37ed8
solotole <solomontoleak47@gmail.com>
2024-01-17 18:40:31 -0800

    function printing characters in characters icharacters in reverse alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-def determinant(s):
-    if s >= 97 and s <= 122:
-        return True
-    if s < 97 or s > 122:
-        return False
-
-
 def uppercase(str):
-    size = len(str)
-    for i in range(size):
-        string = ord(str[i])
-        if (string >= 97 and string <= 122) or (string < 97 or string > 122):
-            if i != (size - 1):
-                print("{:c}".format(string - 32 
-                    if determinant(string) else string), end="")
-            if i == (size - 1):
-                print("{0:c}".format(string - 32))
-        else:
-            pass
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{0:}".format(i), end="")
+    print("")
+
