5c02f561d5511fd8ecae0abb9040ad356a85ec60
solotole <solomontoleak47@gmail.com>
2024-01-18 03:48:54 -0800

    import the file while preventing execution if imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    first_value = 1
-    second_value = 2
-    sum = add(first_value, second_value)
-    print("{0:d} + {1:d} = {2:d}".format(first_value, second_value, sum))
+    a = 1
+    b = 2
+    addition = add(a, b)
+    print("{0:d} + {1:d} = {2:d}".format(a, b, addition))
