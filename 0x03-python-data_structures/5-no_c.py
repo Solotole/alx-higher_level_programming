ef5ab08cfaa4f5c4c484a28030e572fae46e69f9
solotole <solomontoleak47@gmail.com>
2024-01-22 01:39:02 -0800

    function that returns a tuple with the length of a string and its first

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     if my_string != "":
-        new_string = ""
         for i in range(len(my_string)):
             if (my_string[i] != 'c') and (my_string[i] != 'C'):
                 new_string += my_string[i]
             else:
                 continue
+    else:
+        new_string = ""
     return new_string
