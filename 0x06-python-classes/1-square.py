ec62c84e86c407af0d2a48e02bae128a29177905
solotole <solomontoleak47@gmail.com>
2024-02-06 11:32:31 -0800

    class that defines a square by

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""this is class square.
-"""
+"""this is class square."""
 
 class Square:
-    """definition of class Square.
-    """
+    """definition of class Square."""
 
     def __init__(self, size):
         """Automatic call upon instance.
 
         Args:
             size (int): the size of the square.
+
         """
         self.__size = size
