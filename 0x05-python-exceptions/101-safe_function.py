0480b2a2db4a0eb6f3bdb689c09dcaa669243f86
solotole <solomontoleak47@gmail.com>
2024-02-07 16:42:04 -0800

    function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    result = 0
+    try:
+        result = fct(args[0], args[1])
+        return result
+    except (TypeError, ValueError, IndexError, ZeroDivisionError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
