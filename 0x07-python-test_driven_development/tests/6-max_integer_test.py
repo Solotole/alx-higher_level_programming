596f3fbd3e537b9711306b7cc22a78b09c06ffd5
solotole <solomontoleak47@gmail.com>
2024-02-16 01:51:28 -0800

    Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,7 +16,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([0, 0]), 0)
         self.assertEqual(max_integer([0, -12]), 0)
         self.assertEqual(max_integer([0, 78]), 78)
-        self.assertEqual(max_integer([-12, 0, 12]), 12)
+        self.assertEqual(max_integer([-12, 12]), 12)
         self.assertEqual(max_integer([1, -1 , -938, -934]), 1)
     if __name__ == '__main__':
         unittest.main()
