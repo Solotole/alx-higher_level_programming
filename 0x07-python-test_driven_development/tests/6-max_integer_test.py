5b74822cb5cd659aa41fdbcc2a50d67f7fc615c6
solotole <solomontoleak47@gmail.com>
2024-02-16 01:39:29 -0800

    Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """class passing Testcase"""
+    def test_max_integer(self):
+        """method resposible for tesing perfomance of code"""
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([2, 6, 89, 12, 92]), 92)
+    if __name__ == '__main__':
+        unittest.main()
