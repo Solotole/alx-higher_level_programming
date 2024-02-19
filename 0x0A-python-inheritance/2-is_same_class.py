fb11371bed89d2436783508e69a377b1399536ff
solotole <solomontoleak47@gmail.com>
2024-02-18 22:11:54 -0800

    type-to confirm type of an instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module to confirm for an instance"""
+
+
+def is_same_class(obj, a_class):
+    """instance of a class
+    """
+
+    return True if type(obj) == a_class else False
