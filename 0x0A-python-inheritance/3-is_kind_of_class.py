3936b3dfbf747972a0f6122a191ad40e1726cb58
solotole <solomontoleak47@gmail.com>
2024-02-18 22:20:20 -0800

    isinstance-same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """using instance
+    """
+
+    return True if isinstance(obj, a_class) else False
