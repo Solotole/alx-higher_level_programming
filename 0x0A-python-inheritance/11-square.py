c47c31ededb26e7c2a7fab2176b9af3de778d96e
solotole <solomontoleak47@gmail.com>
2024-02-19 17:37:44 -0800

    Square#2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!.usr/bin/python3
+"""class depicting multi level inheitance"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class represetation"""
+    def __init__(self, size):
+        """class square instantiation
+
+        Size (int): size of the square side
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area method
+
+        Returns:
+            return area of the square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """the str method
+
+        Returns:
+            return a string
+        """
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__size) + "/" + str(self.__size)
+        return string
