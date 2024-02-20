e78abdf2e97b03f055b6ff513a40a5370928811b
solotole <solomontoleak47@gmail.com>
2024-02-20 06:52:15 -0800

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,9 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-if len(argv) == 1:
-    save_to_json_file([], "add_item.json")
-else:
-    new_list = []
+try:
     new_list = load_from_json_file("add_item.json")
-    new_list.extend(argv[1:])
-    save_to_json_file(new_list, "add_item.json")
+except FileNotFoundError:
+    new_list = []
+new_list.extend(argv[1:])
+save_to_json_file(new_list, "add_item.json")
