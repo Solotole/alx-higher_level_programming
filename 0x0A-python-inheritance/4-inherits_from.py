53d013bb1176358d06c5d48b0142a6a30874f5d8
solotole <solomontoleak47@gmail.com>
2024-02-19 17:57:45 -0800

    issubclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
         return True if obj is an instance of a class or inherited
         and false otherwise
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
