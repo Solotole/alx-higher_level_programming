e9f43b07819f16f69065c1c4bba47cf1db2093bf
solotole <solomontoleak47@gmail.com>
2024-03-16 07:41:25 -0700

    accessing elemnts in a lists of list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix != [[]]:
         for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                if j != len(matrix) - 1:
+            for j in range(len(matrix[i])):
+                if j != len(matrix[i]) - 1:
                     print("{0:d}".format(matrix[i][j]), end=" ")
                 else:
                     print("{0:d}".format(matrix[i][j]))
