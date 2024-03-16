a5ed37d0b52b1b9f001c73f6d93775ee17e491d1
solotole <solomontoleak47@gmail.com>
2024-03-16 07:11:07 -0700

    accessing an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,6 @@ def element_at(my_list, idx):
     if not my_list:
         if idx < 0:
             return None
-        elif idx >= len(my_list):
+        if idx >= len(my_list):
             return None
-        else:
-            pass
     return my_list[idx]
