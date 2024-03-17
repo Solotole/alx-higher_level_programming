960f271aaca3ae549df6b4965ef6d1faad4540ab
solotole <solomontoleak47@gmail.com>
2024-03-16 22:20:03 -0700

    deleting by value from a dictionary

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    n_list = []
+    if a_dictionary and value is not None:
+        for key, data in a_dictionary.items():
+            n_list.append(key)
+            n_list.append(data)
+        if n_list.count(value) == 0:
+            return a_dictionary
+        elif n_list.count(value) > 0:
+            for nums in range(1, len(n_list), 2):
+                if n_list[nums] == value:
+                    del a_dictionary[n_list[nums - 1]]
+            return a_dictionary
+    else:
+        return
