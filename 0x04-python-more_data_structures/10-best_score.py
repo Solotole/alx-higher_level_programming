cb57c28b88d62fa97de7b5253a2bd722026c3556
solotole <solomontoleak47@gmail.com>
2024-03-16 19:58:26 -0700

    best score in a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        if len(a_dictionary) > 1: # new improvement:check
+        if len(a_dictionary) > 1:
             max_value = max(a_dictionary.values())
             for i in a_dictionary:
                 if a_dictionary[i] == max_value:
