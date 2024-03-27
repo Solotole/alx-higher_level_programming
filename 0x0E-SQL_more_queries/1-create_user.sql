78b3a8d843af59a4f26b2bfa8102704cfb510798
solotole <solomontoleak47@gmail.com>
2024-03-27 04:59:01 -0700

    creating user and granting all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL server user user_0d_1
+-- does not fail if already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
