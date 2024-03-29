bd105db00ad7da26a2251a3649feef9f875ae7c5
solotole <solomontoleak47@gmail.com>
2024-03-28 18:24:45 -0700

    title and summation

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_shows.title
+-- GROUP BY tv_shows.title
 ORDER BY rating_sum DSC
