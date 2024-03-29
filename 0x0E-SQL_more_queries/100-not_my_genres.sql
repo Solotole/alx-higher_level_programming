9cb6c0057a7a9177b7ac94b263fc23d744725823
solotole <solomontoleak47@gmail.com>
2024-03-28 17:15:49 -0700

    NOT-script to generate title without title=Dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,17 @@
 -- script that list all genres not linked to the show Dexter
 SELECT tv_genres.name
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE NOT tv_shows.title = 'Dexiter'
-ORDER BY tv_genres.name ASC
+
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
