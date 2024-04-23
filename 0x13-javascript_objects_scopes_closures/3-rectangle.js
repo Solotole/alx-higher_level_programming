97edc997ae52ceafc6c9872e00183bff78e1ed58
solotole <solomontoleak47@gmail.com>
2024-04-23 12:50:16 -0700

    print() method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// print method introduction
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rowXs = '';
+      for (let j = 0; j < this.width; j++) {
+	rowXs += 'X';
+      }
+      console.log(rowXs);
+    }
+  }
+}
+module.exports = Rectangle;
