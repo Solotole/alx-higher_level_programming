3e785f54aa451c7f851f0348664c8427d42c724d
solotole <solomontoleak47@gmail.com>
2024-04-23 01:15:02 -0700

    concatinating strings

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const newArray = process.argv.slice(1);
+let returnValue;
+function factorial(value) {
+  if (!Number(value)) {
+    return 1;
+  }
+  if (Number(value) === 0 || Number(value) === 1) {
+    return 1;
+  }
+  return value * factorial(value - 1);
+}
+returnValue = factorial(newArray[1]);
+console.log(returnValue);
