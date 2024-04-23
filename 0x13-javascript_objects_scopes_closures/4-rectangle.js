2ab4f86fdf5b4e007fc594d24fe35aeb6c99e6c5
solotole <solomontoleak47@gmail.com>
2024-04-23 13:58:24 -0700

    rotating and doubling dimensions

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+// constrictor method introduction
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rowXs = '';
+      for (let j = 0; j < this.width; j++) {
+	rowXs += 'X';
+      }
+      console.log(rowXs);
+    }
+  }
+
+  rotate () {
+    let newValue = this.width;
+    this.width = this.height;
+    this.height = newValue;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
