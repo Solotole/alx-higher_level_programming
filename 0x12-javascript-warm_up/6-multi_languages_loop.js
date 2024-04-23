a768460b7cfea131bd0697fe9aed894d309cdc89
solotole <solomontoleak47@gmail.com>
2024-04-23 01:23:12 -0700

    looping to print output

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const newArray = [];
+newArray[0] = 'C is fun';
+newArray[1] = 'Python is cool';
+newArray[2] = 'JavaScript is amazing';
+for (let i = 0; i < 3; i++) {
+	console.log(newArray[i]);
+}
