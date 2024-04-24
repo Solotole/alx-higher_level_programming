c487b7ebea2ef30d3df4569604f93792c094a850
solotole <solomontoleak47@gmail.com>
2024-04-23 19:43:42 -0700

    factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const oldList = require('./100-data.js').list;
-let newerArray = oldList.map((value, index) => value * index);
+const newerArray = oldList.map((value, index) => value * index);
 console.log(oldList);
 console.log(newerArray);
