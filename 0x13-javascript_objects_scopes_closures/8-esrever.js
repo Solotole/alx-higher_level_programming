266bda04d292898539dfeae53d9df6f4fb5eb9bc
solotole <solomontoleak47@gmail.com>
2024-04-23 20:05:39 -0700

    reversing list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const listLength = list.length - 1;
   let newListIndex = 0;
-  let newList = [];
+  const newList = [];
   for (let i = listLength; i >= 0; i--) {
     newList[newListIndex] = list[i];
     newListIndex += 1;
