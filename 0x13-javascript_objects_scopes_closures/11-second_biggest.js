4c652341c30b642e41eea7ce29e4254fe702d67c
solotole <solomontoleak47@gmail.com>
2024-04-24 19:55:36 -0700

    sorting for second biggest number

--- 0x13-javascript_objects_scopes_closures/11-second_biggest.js
+++ 0x13-javascript_objects_scopes_closures/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number); // Convert arguments to numbers
-
+const args = process.argv.slice(2).map(Number);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  const sortedArgs = args.sort((a, b) => b - a); // Sort arguments in descending order
-  console.log(sortedArgs[1]); // Print the second element in the sorted array
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
