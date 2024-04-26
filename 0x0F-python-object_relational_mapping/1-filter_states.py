c94e4466b645a40ad87ae8ab1471cd71b21d2c16
solotole <solomontoleak47@gmail.com>
2024-04-26 06:42:06 -0700

    filter by state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that lists all states with
-a name starting with N from the database
-hbtn_0e_0_usa
+   a name starting with N from the database
+   hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
