fd24847b0b9b1558860ae4f937728a500c6a4c52
solotole <solomontoleak47@gmail.com>
2024-04-26 02:00:59 -0700

    lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,14 @@ if __name__ == "__main__":
     password = argv[2]
     db_name = argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=password, db=db_name, charset="utf8")
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=user_name,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+            )
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
