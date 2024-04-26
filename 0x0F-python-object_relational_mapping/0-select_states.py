f5e88c622c84493ba6d098fb487e8f64f357f341
solotole <solomontoleak47@gmail.com>
2024-04-26 01:19:55 -0700

    lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,22 @@
 
 if __name__ == "__main__":
     from sys import argv
+    if len(argv) < 4:
+        print("Error: reuqirement is 3 arguments")
+        sys.exit()
     import MySQLdb
     user_name = argv[1]
     password = argv[2]
     db_name = argv[3]
+    host = "localhost"
+    port = 3306
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=password, db=db_name, charset="utf8")
+    conn = MySQLdb.connect(host, port, user_name, password, db_name)
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
+    conn.clode()
+    # cur.close()
 
     for state in states:
         print(state)
-    cur.close()
-    conn.close()
