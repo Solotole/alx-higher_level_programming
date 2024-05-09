b9f8a53def5d194556076286f7b27058e80cd9a9
solotole <solomontoleak47@gmail.com>
2024-05-09 16:41:46 -0700

    unsorted list peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,37 +2,23 @@
 """ script finding peak in an unsorted list """
 
 
-def max_value(list_l):
-    """ function to find maximum value
-    Args:
-        list_l (list): finding maximum value in a list
-    """
-    if list_l[0] > list_l[1]:
-        return list_l[0]
-    elif list[0] < list_l[1]:
-        return list_l[1]
-    else:
-        return list_l[0]
-
-
 def find_peak(list_of_integers):
     """ function finding peak
     Args:
         list_of_integers (list): list of unsorted list
     """
-    list_l = list_of_integers
-    length = len(list_l)
-    mid = int(length / 2)
-    if list_l == []:
+    list_copy = list_of_integers
+    if not list_copy
         return None
-    if length == 1:
-        return list_l[0]
-    if length == 2:
-        return max_val(list_l)
-    if list_l[mid] > list_l[mid - 1] and \
-            list_l[mid] < list_l[mid + 1]:
-        return list_l[mid]
-    if list_l[mid] > list_l[mid + 1]:
-        return find_peak(list_l[mid + 1:])
-    if list_l[mid] < list_l[mid - 1]:
-        return find_peak(list_l[:mid])
+    length = len(list_copy)
+
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if list_copy[mid] > list_copy[mid - 1] and list_copy[mid] > list_copy[mid + 1]:
+            return list_copy[mid]
+        if list_copy[mid - 1] > list_copy[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return list_[start]
