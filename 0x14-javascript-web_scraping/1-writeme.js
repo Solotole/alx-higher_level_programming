c75f6dd18bacc994ebe3c7a77e00514dec39875a
Solotole <solomontoleak47@gmail.com>
2024-06-04 10:44:04 +0100

    writting to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // writing a string to a file
 const fs = require('fs');
-const file_path = process.argv[2]
-const content = process.argv[3]
+const filepath = process.argv[2];
+const content = process.argv[3];
 
-fs.writeFile(file_path, content, 'utf-8', (error) => {
+fs.writeFile(filepath, content, 'utf-8', (error) => {
   if (error) {
     console.error(error);
   }
