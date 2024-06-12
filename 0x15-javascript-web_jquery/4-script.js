47d6d97658b61afbaff4f8c289ac209b0817cf06
Solotole <solomontoleak47@gmail.com>
2024-06-12 15:05:42 +0100

    toggle over classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+$('DIV#toggle_header').click(function () {
+  if ($('header').hasClass('green')) {
+    $('header').removeClass('green').addClass('red');
+  } else if ($('header').hasClass('red')) {
+    $('header').removeClass('red').addClass('green');
+  }
+});
