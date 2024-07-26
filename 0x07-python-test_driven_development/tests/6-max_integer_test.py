078f2331193ee018f39924f2f2bdcb8a7f038d88
Solotole <solomontoleak47@gmail.com>
2024-07-26 09:08:15 +0100

    mandatarory task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,22 +4,61 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    """class passing Testcase"""
+
+    """Suite test for max_integer function"""
+
     def test_max_integer(self):
-        """method resposible for tesing perfomance of code"""
+        self.assertEqual(max_integer([5, -2, 100, 3]), 100)
+
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([2, 6, 89, 12, 92]), 92)
-        self.assertEqual(max_integer([1]), 1)
-        self.assertEqual(max_integer([1, 1]), 1)
-        self.assertEqual(max_integer([0]), 0)
+
+    def test_repeated_number(self):
+        self.assertEqual(max_integer([1000, 1000, 1000]), 1000)
+
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([50, 51, 50, 49]), 51)
+
+    def test_max_operated_integer(self):
+        self.assertEqual(max_integer([-3, -5 * -5, 12, -1]), 25)
+
+    def test_neg_numbers(self):
+        self.assertEqual(max_integer([-10, -5, -2, -1]), -1)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
+
+    def test_zero_number(self):
         self.assertEqual(max_integer([0, 0]), 0)
-        self.assertEqual(max_integer([0, -12]), 0)
-        self.assertEqual(max_integer([0, 78]), 78)
-        self.assertEqual(max_integer([-12, 12]), 12)
-        self.assertEqual(max_integer([-1, -76, -100, -45]), -1)
-        self.assertEqual(max_integer([1, -1 , -938, -934]), 1)
-        self.assertEqual(max_integer([-2, 0, 34, 34, 35]), 35)
-        self.assertEqual(max_integer([34, 0, -4.08]), 34)
-    if __name__ == '__main__':
-        unittest.main()
+
+    def test_big_list(self):
+        self.assertEqual(max_integer([
+            901, 902, 903, 904, 905, 906, 907, 908, 909, 910,
+            911, 912, 913, 914, 915, 916, 917, 918, 919, 920,
+            919, 918, 917, 1000, 915, 914, 913, 912, 911, 910,
+            909, 908, 907, 906, 905, 904, 903, 902, 901]), 1000)
+
+    def test_list_with_loop(self):
+        my_list = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer([i * 5 for i in my_list]), 25)
+
+    def test_one_number_in_a_list(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_string_number_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([0, '1'])
+
+    def test_tuple_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([10, (20, 30)])
+
+    def test_dictionary(self):
+        with self.assertRaises(KeyError):
+            max_integer({'key1': 1, 'key2': 2})
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+            max_integer(1
